71c4b194b3a8699946fd7d368dbac91461336ecb
lzt <842835321@qq.com>
2022-07-14 08:56:43 +0800

    实现一个Compose函数

---
+++ day10-compose.js
@@ -0,0 +1,38 @@
+// 实现一个 compose 函数
+// 用法如下:
+// function fn1(x) {
+//     return x + 1;
+// }
+// function fn2(x) {
+//     return x + 2;
+// }
+// function fn3(x) {
+//     return x + 3;
+// }
+// function fn4(x) {
+//     return x + 4;
+// }
+// const a = compose(fn1, fn2, fn3, fn4);
+// console.log(a(1)); // 1+4+3+2+1=11
+
+function fn1(x) {
+    return x + 1;
+}
+function fn2(x) {
+    return x + 2;
+}
+function fn3(x) {
+    return x + 3;
+}
+function fn4(x) {
+    return x + 4;
+}
+const compose = (...fns) => {
+    if (!fns) return v => v;
+    if (fns.length == 1) return fns[0];
+    return fns.reduce((prev, cur) => (...args) => prev(cur(...args)))
+}
+
+const a = compose(fn1, fn2, fn3, fn4);
+console.log(a(1)); // 1+4+3+2+1=11
+
