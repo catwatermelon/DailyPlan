f373e3a0865e594c5fc90ba4239cd7ad404c0dd1
lzt <842835321@qq.com>
2022-07-14 12:46:22 +0800

    Promise.all规则补充

--- day16-Promise.all.js
+++ day16-Promise.all.js
@@ -1,9 +1,15 @@
 // 实现一个 Promise.all
+// 如果传入的参数是一个空的可迭代对象，那么此promise对象回调完成(resolve),只有此情况，是同步执行的，其它都是异步返回的。
+// 如果传入的参数不包含任何 promise，则返回一个异步完成.
+// promises 中所有的promise都“完成”时或参数中不包含 promise 时回调完成。
+// 如果参数中有一个promise失败，那么Promise.all返回的promise对象失败
+// 在任何情况下，Promise.all 返回的 promise 的完成状态的结果都是一个数组
+
 Promise.all = (promises) => {
     const lens = promises.length;
-
-    if(!lens) return [];
     const result = [];
+    if(!lens) return Promise.resolve(result);
+
     let cnt = 0;
     return new Promise((resolve, reject) => {
         for(let i=0; i<lens; ++i) {
