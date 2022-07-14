d0dbe675bfa3204f9001a0994a5497d6367d66fa
lzt <842835321@qq.com>
2022-07-14 12:43:51 +0800

    修复实现一个Promise.all的问题

--- day16-Promise.all.js
+++ day16-Promise.all.js
@@ -7,7 +7,8 @@ Promise.all = (promises) => {
     let cnt = 0;
     return new Promise((resolve, reject) => {
         for(let i=0; i<lens; ++i) {
-            promises.then(res=>{
+            // 使用Promise.resolve包装，兼容非Promise的情况
+            Promise.resolve(promises[i]).then(res=>{
                 result[i] = res; // Promise.all返回的数组顺序是和传入的顺序一样的
                 cnt++;
                 if(cnt == lens) {
