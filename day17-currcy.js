7b7ce83dee8ecc5b0e18d0f4be2168c55a7b0e67
lzt <842835321@qq.com>
2022-07-14 16:19:07 +0800

    函数柯里化优化版

--- day17-currcy.js
+++ day17-currcy.js
@@ -5,15 +5,15 @@
 // console.log(sumFn(1)(2)(3)); //6
 // console.log(sumFn(1)(2, 3)); //6
 // 柯里化的作用：参数复用、提前返回和 延迟执行
-function sum(a,b,c) {
-    return a+b+c;
+function sum(a, b, c) {
+    return a + b + c;
 }
 function curry(fn, args = []) {
     // 返回一个函数
-    return function(...subArgs) {
+    return function (...subArgs) {
         const rest = [...args, ...subArgs];
         // 判断当前已接收参数数量是否等于函数fn的所有参数数量
-        if(rest.length < fn.length) { // 小于则继续返回一个 curry 函数
+        if (rest.length < fn.length) { // 小于则继续返回一个 curry 函数
             return curry.call(this, fn, rest);
         } else { // 当参数接收足够时，执行该函数并返回结果
             return fn.apply(this, rest);
@@ -21,11 +21,37 @@ function curry(fn, args = []) {
     }
 }
 
-let sumFn = curry(sum);
-console.log(sumFn(1)(2)(3)); //6
-console.log(sumFn(1)(2, 3)); //6
+let sumFn11 = curry(sum);
+console.log(sumFn11(1)(2)(3)); //6
+console.log(sumFn11(1)(2, 3)); //6
 
 
-let sumFn2 = curry(sum, [1]);
-console.log(sumFn2(2)(3)); //6
-console.log(sumFn2(2, 3)); //6
+let sumFn12 = curry(sum, [1]);
+console.log(sumFn12(2)(3)); //6
+console.log(sumFn12(2, 3)); //6
+
+// curry2(sum) curry2(sum， 1) curry2(sum, [1])
+function curry2(fn, args = []) {
+    return (...subArgs) => {
+        const rest = [].concat(args, subArgs);
+        if (rest.length >= fn.length) {
+            return fn.call(this, ...rest);
+        } else {
+            return curry.call(this, fn, rest);
+        }
+    }
+}
+
+let sumFn21 = curry2(sum);
+console.log(sumFn21(1)(2)(3)); //6
+console.log(sumFn21(1)(2, 3)); //6
+
+
+let sumFn22 = curry2(sum, 1);
+console.log(sumFn22(2)(3)); //6
+console.log(sumFn22(2, 3)); //6
+
+
+let sumFn23 = curry2(sum, [1]);
+console.log(sumFn23(2)(3)); //6
+console.log(sumFn23(2, 3)); //6
