1d157079502a7093afd2794b6a8af532aec1110f
lzt <842835321@qq.com>
2022-07-14 15:15:37 +0800

    函数柯里化

---
+++ day17-currcy.js
@@ -0,0 +1,24 @@
+// 实现一个curry函数，将普通函数进行柯里化
+// sum(1)(2)(3) 返回结果是1,2,3之和
+// console.log(sumFn(1)(2)(3)); //6
+// console.log(sumFn(1)(2, 3)); //6
+
+function sum(a,b,c) {
+    return a+b+c;
+}
+function curry(fn, args = []) {
+    // 返回一个函数
+    return function(...subArgs) {
+        const rest = [...args, ...subArgs];
+        // 判断当前已接收参数数量是否等于函数fn的所有参数数量
+        if(rest.length < fn.length) { // 小于则继续返回一个 curry 函数
+            return curry.call(this, fn, rest);
+        } else { // 当参数接收足够时，执行该函数并返回结果
+            return fn.apply(this, rest);
+        }
+    }
+}
+
+let sumFn = curry(sum);
+console.log(sumFn(1)(2)(3)); //6
+console.log(sumFn(1)(2, 3)); //6
