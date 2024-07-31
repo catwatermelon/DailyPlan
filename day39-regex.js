1a9fccdc0c36b712b7fe4de56495287425d4acda
catwatermelon <842835321@qq.com>
2024-07-31 16:50:26 +0800

    feat: 正则金额练习

--- day39-regex.js
+++ day39-regex.js
@@ -26,3 +26,15 @@ var str = 'The fat cat sat on the mat.'
 
 const regex = /(?<!(T|t)he\s)(fat|cat)/
 console.log(str.replace(regex, '---'))
+
+
+// 金额千分位/(\d)(?=(\d{3})+((\.\d+)$))/g
+// 这是一个复杂的正则表达式它的大致含义如下：
+// (\d) ：匹配一个数字并将其捕获到分组 1 中。
+// (?=(\d{3})+((\.\d+)$)) ：这是一个正向前瞻断言，它要求当前位置后面存在以下内容：
+// (\d{3})+ ：一个或多个连续的三位数字。
+// ((\.\d+)$) ：以一个点和至少一位数字结尾。
+
+var str = '1234567.234';
+const amontRegex = /(\d)(?=(\d{3})+((\.\d+)$))/g
+console.log(str.replace(amontRegex, '$1,'));
